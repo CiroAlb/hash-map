52c854f53c133446babbf44a14a40e8a2a15d9fa
Borg <128256628+CiroAlb@users.noreply.github.com>
2024-10-31 11:16:01 -0300

    modificar server

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
 
-// Crea una aplicación de Express
 const app = express();
 
-// Sirve los archivos estáticos (HTML, JS, CSS) desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Escucha en el puerto 3000 o cualquier puerto especificado en el entorno
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
